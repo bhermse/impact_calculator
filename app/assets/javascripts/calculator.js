3b1cecd28224da30c120e896393c4f56e0b123e5
bryan <bryan.hermsen@nebraska.gov>
2017-09-22 08:56:20 -0500

    Functions to update presets based on basic info

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -3,23 +3,30 @@ $(function () {
         e.preventDefault();
         $('html, body').animate({scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
-    $('#electricity, #gasoline').slider({
+
+    $('#electricitySlider, #gasolineSlider').slider({
         formatter: function (value) {
             return 'Current value: ' + value;
         }
     });
+
     $('#electricity').on('slide', function (slideEvt) {
         $("#electricityCurrentSliderValLabel").text(slideEvt.value);
     });
+
     $('#gasoline').on('slide', function (slideEvt) {
         $("#gasolineCurrentSliderValLabel").text(slideEvt.value);
     });
-    $("#calculate_button").click(function() {
+
+    $("#calculate_button, #calculate").click(function() {
         var form_values = $('#basic_questions_form').serialize();
+        $('div.spending.explanation').hide();
+
         $.ajax({
+            beforeSend: function() {$('.calculating').fadeIn('slow');},
             url: '/calculator/calculate',
             type: "POST",
-            dataType: "JSON",
+            dataType: "script",
             data: form_values
         })
     })
---
+++ app/helpers/calculator_helper.rb
@@ -0,0 +1,5 @@
+module CalculatorHelper
+  def spending_explanation(electricity, gas)
+    "Households similar to your spent approximately $#{electricity}/month on electricity and $#{gas}/month on gasoline. If this doesn't align with your spending, make adjustments here."
+  end
+end
